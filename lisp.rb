0cb063839ae34b8ca5d30bf141cc80ccbc0c7b92
James Moriarty <jamespaulmoriarty@gmail.com>
2014-06-02 18:15:44 +1200

    atoms, eval, and execute.

--- lisp.rb
+++ lisp.rb
@@ -2,6 +2,10 @@
 
 class Lisp
   class << self
+    def eval(string)
+      execute parse(tokenize(string))
+    end
+
     def tokenize(string)
       string.gsub('(',' ( ').gsub(')',' ) ').split
     end
@@ -10,7 +14,52 @@ class Lisp
       _parse(tokens)[0]
     end
 
+    def execute(expression, env = env)
+      return expression unless expression.is_a?(Array)
+
+      # depth first traversal of representation
+      # [:*, 2, [:+, 1, 0]]
+      # [:+, 1, 0]
+      expression = expression.map do |atom|
+        case atom
+        when Array
+          execute atom
+        else
+          atom
+        end
+      end
+
+      # depth first execution
+      # [:*, 2, [:+, 1, 0]]
+      # [:*, 2, 1]
+      atom = expression.shift
+      case atom
+      when Symbol
+        function  = env[atom]
+        arguments = expression
+        function.call(*arguments.flatten)
+      else
+        expression
+      end
+    end
+
     private
+
+    def env
+      {
+        :+ => Proc.new { |*args| args.inject(0, &:+) },
+        :* => Proc.new { |*args| args.inject(1, &:*) }
+      }
+    end
+
+    def atom(token)
+      case token
+      when /\d/
+        token.to_f
+      else
+        token.to_sym
+      end
+    end
 
     def _parse(tokens, representation = [])
       token = tokens.shift
@@ -23,7 +72,7 @@ class Lisp
       when nil
         representation
       else
-        _parse tokens, representation.push(token)
+        _parse tokens, representation.push(atom(token))
       end
     end
   end
@@ -40,7 +89,7 @@ class TestLisp < Minitest::Test
   end
 
   def test_parse
-    assert_equal ["*", "2", ["+", "1", "0"]], Lisp.parse(Lisp.tokenize("(* 2 (+ 1 0) )"))
+    assert_equal [:*, 2, [:+, 1, 0]], Lisp.parse(Lisp.tokenize("(* 2 (+ 1 0) )"))
   end
 
   # representation
